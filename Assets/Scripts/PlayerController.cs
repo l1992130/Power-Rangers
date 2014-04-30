11608774cc0e1e627e2828b8391c6e5d916bd4a7
l1992130 <l1992130@163.com>
2014-04-30 18:12:06 +0800

    切图，动画制作

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,9 @@ public class PlayerController : MonoBehaviour
 	public bool jump = false;  //能否条约
 	public bool kick = false;
 	public bool sit = false;
+	public bool boxing = false;
 	public float moveForce = 365f;  //移动给的力
-	public float maxSpeed = 5f;  //最大移动速度
+	public float maxSpeed = 2f;  //最大移动速度
 	public AudioClip[] jumpClips; //跳跃时候的声音
 	public float jumpForce = 1000f; //跳跃时候的力度
 
@@ -18,11 +19,17 @@ public class PlayerController : MonoBehaviour
 	private bool grounded = false; //是否着地
 	private Animator anim; //引用角色动画
 
+	//腿部攻击相关参数
 	private float timeSinceLastKick = 0f;
-	private bool isKeepPress = false;
+	private bool isKickKeepPress = false;
+
+	//下蹲系列动作相关
 
 	private AnimatorStateInfo stateInfo;
 
+	//手部攻击相关参数
+	private float timeSinceLastBoxing = 0f;
+	private bool isBoxingKeepPress = false;
 	// Use this for initialization
 	void Start () 
 	{
@@ -34,30 +41,52 @@ public class PlayerController : MonoBehaviour
 		groundCheck = transform.Find("groundCheck");
 
 		anim = GetComponent<Animator>();
-		stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-//		print("position:"+transform.position);
-//		print("groundCheck:"+groundCheck.position);
+		stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+		//跳
 		grounded = Physics2D.Linecast(transform.position,groundCheck.position,1 << LayerMask.NameToLayer("Ground"));
-//		print ("ground" + grounded);
-		if (Input.GetButtonDown ("PGJump") && grounded)
-					jump = true;
-		if (Input.GetButtonDown ("PGKick"))// && grounded)
+		if (Input.GetButtonDown("PGJump") && grounded) 
+		{
+			print("jumpbutton"+ Time.time);
+			jump = true;
+			print(anim.GetLayerName(0));
+		}
+		//踢腿
+		if (Input.GetButtonDown ("PGKick"))
 		{
 			kick = true;
 		}
-		if (Input.GetKey(KeyCode.S))
-			sit = true;
-		if (Input.GetKeyUp (KeyCode.S))
-						sit = false;
-		print ("Sit:"+sit);
 		if (anim.GetInteger ("Kick") != 0)
 			anim.SetInteger ("Kick", 0);
+		//下蹲
+		if (Input.GetAxis ("PGVertical") < 0 && Input.GetButton ("PGVertical")) 
+		{
+			sit = true;
+		}
+		if (Input.GetButtonUp ("PGVertical")) 
+		{
+			sit = false;
+		}
+		//打拳
+		if (Input.GetButtonDown ("PGBoxing"))
+		{
+			boxing = true;
+		}
+		if (anim.GetInteger ("Boxing") != 0)
+			anim.SetInteger ("Boxing", 0);
 
+		//除了跳和正常跑，其余速度降低
+		if (getButton("PGHorizontal") && !stateInfo.IsName ("PGRedRun") && !stateInfo.IsName("PGRedJump"))
+			maxSpeed = 0.1f;
+		if (stateInfo.IsName("PGRedRun"))
+			maxSpeed = 2f;
+
+		//左右跑动
 		float h = Input.GetAxis ("PGHorizontal");
 		anim.SetFloat ("Speed", Mathf.Abs (h));
 
@@ -69,10 +98,14 @@ public class PlayerController : MonoBehaviour
 			Flip ();
 		else if (h < 0 && facingRight)
 			Flip ();
+	}
+
+	void FixedUpdate()
+	{
 		if (jump)
 		{
 			anim.SetTrigger("Jump");
-//			anim.Play("PGRedAttackKick");
+			//			anim.Play("PGRedAttackKick");
 			rigidbody2D.AddForce(new Vector2(0f,jumpForce));
 			jump = false;
 		}
@@ -82,17 +115,15 @@ public class PlayerController : MonoBehaviour
 			Debug.Log("okok:"+timeSinceLastKick);
 			if (timeSinceLastKick < 0.5 && timeSinceLastKick > 0)
 			{
-				isKeepPress = true;
+				isKickKeepPress = true;
 			}
-			if (isKeepPress)
+			if (isKickKeepPress)
 			{
-				print("keepkick");
 				anim.SetInteger("Kick",2);
-				isKeepPress = false;
+				isKickKeepPress = false;
 			}
 			else
 			{
-				print("kick");
 				anim.SetInteger("Kick",1);
 			}
 			timeSinceLastKick = Time.time;
@@ -106,8 +137,35 @@ public class PlayerController : MonoBehaviour
 		{
 			anim.SetBool("Sit",false);
 		}
+		if (boxing)
+		{	
+			timeSinceLastBoxing = Time.time - timeSinceLastBoxing;
+			Debug.Log("okok:"+timeSinceLastBoxing);
+			if (timeSinceLastBoxing < 0.5 && timeSinceLastBoxing > 0)
+			{
+				isBoxingKeepPress = true;
+			}
+			if (isBoxingKeepPress)
+			{
+				anim.SetInteger("Boxing",2);
+				isBoxingKeepPress = false;
+			}
+			else
+			{
+				anim.SetInteger("Boxing",1);
+			}
+			timeSinceLastBoxing = Time.time;
+			boxing = false;
+		}
+//		if (boxing)
+//		{
+//			anim.SetInteger("Boxing",1);
+//			boxing = false;
+//		}
+//		else
+//			anim.SetInteger("Boxing",0);
 	}
-	
+
 	void Flip()
 	{
 		facingRight = !facingRight;
@@ -115,5 +173,10 @@ public class PlayerController : MonoBehaviour
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+	}
+
+	bool getButton(string buttonName)
+	{
+		return Input.GetButton (buttonName) || Input.GetButtonDown (buttonName);
 	}
 }
