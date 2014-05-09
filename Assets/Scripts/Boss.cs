21bdf097ccd14f9833209bd605019183585d3076
l1992130 <l1992130@163.com>
2014-05-09 17:28:23 +0800

    bossAI完善

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class Boss : MonoBehaviour 
 {
-	public enum BossState {IDLE,WALK,JUMP,ATTACK,HURT,DIE};
+	public enum BossState {IDLE,WALK,JUMP,ATTACK,HURT,DIE,SHOCKWAVE,IMPACT};
 	public float addForce = 25;
 	public float minDistance = 5f;
 	public float shockWaveDistance = 3f;
+	public float impactDistance = 1.5f;
 	public float maxSpeed = 1f;
 	public float changeProbability = 75f;
 	public bool facingRight;
+	public BossState bossState;
 
 	private Transform player;
 	private Animator anim;
+	private AnimatorStateInfo stateInfo;
+	private float timeSinceLastShockWave = 0f;
 	// Use this for initialization
 	void Start () 
 	{
@@ -23,33 +27,100 @@ public class Boss : MonoBehaviour
 	{
 		player = GameObject.FindGameObjectWithTag ("player").transform;
 		anim = GetComponent<Animator>();
+		bossState = BossState.IDLE;
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
+		if (!anim.GetBool ("Walk"))
+						print ("walkfalse");
+		stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 		facingRight = Mathf.Sign (player.transform.position.x - transform.position.x) > 0 ? true : false;
 		Flip (facingRight);
 
-		print ("facing:"+facingRight);
 		float dist = Vector3.Distance (player.transform.position, transform.position);
 		print ("distance:"+dist);
 		if (dist < minDistance) 
 		{
-			anim.SetBool ("ShockWave", true);
-			//anim.SetBool ("Walk", true);
+			if (bossState == BossState.IDLE || (bossState != BossState.SHOCKWAVE && bossState != BossState.IMPACT))
+				anim.SetBool("Walk",true);
+			//print("enterMinDistance");
+			if (dist > shockWaveDistance)
+			{
+				shockWave(1);
+			}
+			else if (dist > impactDistance)
+			{
+				impact(1);
+			}
+			else
+			{
+				bossState = BossState.WALK;
+			}
+
+		}
+		else
+		{
+			bossState = BossState.IDLE;
+		}
+
+		switch (bossState)
+		{
+		case BossState.WALK :
+			anim.SetBool ("Walk", true);
 			rigidbody2D.AddForce (new Vector2 (addForce * Mathf.Sign (player.transform.position.x - transform.position.x), 0));
 			if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)				
 				rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+			//print("walk:"+rigidbody2D.velocity.x+"force:"+Mathf.Sign (player.transform.position.x - transform.position.x));
+			break;
+		case BossState.SHOCKWAVE :
+			//print("shockwave");
+			if (anim.GetBool("Walk"))
+				anim.SetBool ("Walk", false);
+			anim.SetTrigger ("ShockWave");
+			break;
+		case BossState.IMPACT :
+			//print("impact");
+			if (anim.GetBool("Walk"))
+				anim.SetBool ("Walk", false);
+			anim.SetTrigger ("Impact");
+			break;
+		case BossState.IDLE :
+			//print("idle");
+			anim.SetBool("Walk",false);
+			break;
 		}
-		else if (dist>shockWaveDistance)
+	}
+
+	void shockWave(float waitTime) 
+	{
+		timeSinceLastShockWave += Time.deltaTime;
+		if (timeSinceLastShockWave > waitTime)
 		{
+			print("time:"+timeSinceLastShockWave);
 			float random =  Random.Range(1,100);
-			if (random > 0)
-			{
-				anim.SetBool ("ShockWave", true);
-			}
+			if (random > changeProbability)
+				bossState = BossState.SHOCKWAVE;
+			else
+				bossState = BossState.WALK;
+			timeSinceLastShockWave = 0;
 		}
+	}
+
+	void impact (float waitTime)
+	{
+//		timeSinceLastShockWave += Time.deltaTime;
+//		if (timeSinceLastShockWave > waitTime)
+//		{
+//			print("time:"+timeSinceLastShockWave);
+//			float random =  Random.Range(1,100);
+//			if (random > changeProbability)
+//				bossState = BossState.SHOCKWAVE;
+//			else
+//				bossState = BossState.WALK;
+//			timeSinceLastShockWave = 0;
+//		}
 	}
 
 	void Flip(bool faceRight)
